e2804089a5a779e3c779d145d40b3edbc84b97b7
Fahad-smto <simanto.smto.prog@gmail.com>
2026-05-16 23:40:13 +0600

    added

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express, { type Application, type Request, type Response } from 'express'
 const app :Application = express()
-const port = 5000
+const port = 8000
 
 app.use(express.json())
 
